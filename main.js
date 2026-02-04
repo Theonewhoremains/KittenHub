f06383171bec682c9cc9ef4899324c63c620eabc
Theonewhoremains <158802133+Theonewhoremains@users.noreply.github.com>
2026-02-04 19:21:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// Mobile menu (clean, no glitches)
+const toggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+if (toggle) {
+  toggle.addEventListener('click', () => {
+    menu.classList.toggle('open');
+  });
+}
