731834cdb96c878171a7d5aa58b8d848ed2fafb2
Theonewhoremains <158802133+Theonewhoremains@users.noreply.github.com>
2026-02-04 04:49:04 -0800

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.nav ul');
-
-menuToggle.addEventListener('click', () => {
-  nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
-});
